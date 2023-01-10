b4bb099389b7ad2345cbda0a7397c8414c0c7a81
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-01-10 11:57:59 +0100

    Adds fetch books

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
+import "./App.css"
 
 function App() {
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const apiUrl = process.env.REACT_APP_BE_URL
+      const resp = await fetch(`${apiUrl}/books`)
+      const data = await resp.json()
+      console.log(data)
+      setBooks(data.booksArray)
+    }
+    fetchData()
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Books in the catalogue</h1>
+        {books.map((book, index) => (
+          <div key={index}>
+            <img alt="" src={book.img} style={{ width: "200px" }}></img>
+            <span>{book.title}</span>
+          </div>
+        ))}
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
